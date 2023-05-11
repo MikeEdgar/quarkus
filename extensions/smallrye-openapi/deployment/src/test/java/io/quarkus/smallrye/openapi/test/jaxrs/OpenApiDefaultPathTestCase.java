4af8b17fb24ef4207b56910f1877e2ee6c5a29c1
Michael Edgar <michael@xlate.io>
2023-05-11 11:14:39 -0400

    OpenAPI: refine OpenApiHandler method and Accept header handling
    - Set response status for `OPTIONS` request
    - Respond with status `405` for unsupported HTTP methods
    - Refine `Accept` header processing to consider content-type weight
    parameter
    
    Signed-off-by: Michael Edgar <michael@xlate.io>

--- extensions/smallrye-openapi/deployment/src/test/java/io/quarkus/smallrye/openapi/test/jaxrs/OpenApiDefaultPathTestCase.java
+++ extensions/smallrye-openapi/deployment/src/test/java/io/quarkus/smallrye/openapi/test/jaxrs/OpenApiDefaultPathTestCase.java
@@ -4,11 +4,13 @@ import org.hamcrest.Matchers;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import io.quarkus.test.QuarkusUnitTest;
 import io.restassured.RestAssured;
 
-public class OpenApiDefaultPathTestCase {
+class OpenApiDefaultPathTestCase {
     private static final String OPEN_API_PATH = "/q/openapi";
 
     @RegisterExtension
@@ -18,17 +20,61 @@ public class OpenApiDefaultPathTestCase {
                     .addAsResource(new StringAsset("quarkus.smallrye-openapi.store-schema-directory=target"),
                             "application.properties"));
 
+    @ParameterizedTest
+    @CsvSource({
+            // YAML default
+            "GET, " + OPEN_API_PATH + ", '', '', 'application/yaml;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ", '', '', 'application/yaml;charset=UTF-8'",
+            // YAML by Accept header (accepts both, prefers YAML)
+            "GET, " + OPEN_API_PATH + ", 'application/json;q=0.9, application/yaml', '', 'application/yaml;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ", 'application/json;q=0.9, application/yaml', '', 'application/yaml;charset=UTF-8'",
+            // YAML by Accept header (accepts only YAML)
+            "GET, " + OPEN_API_PATH + ", 'application/yaml', '', 'application/yaml;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ", 'application/yaml', '', 'application/yaml;charset=UTF-8'",
+            // YAML by Accept header with unsupported value
+            "GET, " + OPEN_API_PATH + ", 'text/plain', '', 'application/yaml;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ", 'text/plain', '', 'application/yaml;charset=UTF-8'",
+            // YAML by `format` query parameter
+            "GET, " + OPEN_API_PATH + ", '', 'YAML', 'application/yaml;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ", '', 'YAML', 'application/yaml;charset=UTF-8'",
+            // YAML by file extension
+            "GET, " + OPEN_API_PATH + ".yaml, '', '', 'application/yaml;charset=UTF-8'",
+            "GET, " + OPEN_API_PATH + ".yml, '', '', 'application/yaml;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ".yaml, '', '', 'application/yaml;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ".yml, '', '', 'application/yaml;charset=UTF-8'",
+            // JSON by Accept header (accepts both, JSON first)
+            "GET, " + OPEN_API_PATH + ", 'application/json, application/yaml', '', 'application/json;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ", 'application/json, application/yaml', '', 'application/json;charset=UTF-8'",
+            // JSON by `format` query parameter
+            "GET, " + OPEN_API_PATH + ", '', 'JSON', 'application/json;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ", '', 'JSON', 'application/json;charset=UTF-8'",
+            // JSON by file extension
+            "GET, " + OPEN_API_PATH + ".json, '', '', 'application/json;charset=UTF-8'",
+            "HEAD, " + OPEN_API_PATH + ".json, '', '', 'application/json;charset=UTF-8'",
+    })
+    void testOpenApiPathContentTypeVariations(String httpMethod, String path, String acceptHeader, String formatParam,
+            String expectedContentType) {
+        var request = RestAssured.given();
+
+        if (!acceptHeader.isBlank()) {
+            request = request.header("Accept", acceptHeader);
+        }
+
+        if (!formatParam.isBlank()) {
+            request = request.queryParam("format", formatParam);
+        }
+
+        var response = request.request(httpMethod, path);
+
+        response.then().header("Content-Type", expectedContentType);
+
+        if ("HEAD".equals(httpMethod)) {
+            response.then().body(Matchers.is(Matchers.emptyOrNullString()));
+        }
+    }
+
     @Test
-    public void testOpenApiPathAccessResource() {
-        RestAssured.given().header("Accept", "application/yaml")
-                .when().get(OPEN_API_PATH)
-                .then().header("Content-Type", "application/yaml;charset=UTF-8");
-        RestAssured.given().queryParam("format", "YAML")
-                .when().get(OPEN_API_PATH)
-                .then().header("Content-Type", "application/yaml;charset=UTF-8");
-        RestAssured.given().header("Accept", "application/json")
-                .when().get(OPEN_API_PATH)
-                .then().header("Content-Type", "application/json;charset=UTF-8");
+    void testOpenApiPathAccessResource() {
         RestAssured.given().queryParam("format", "JSON")
                 .when().get(OPEN_API_PATH)
                 .then()
@@ -39,9 +85,20 @@ public class OpenApiDefaultPathTestCase {
                 .body("paths.'/resource'.get.servers[0]", Matchers.hasKey("url"))
                 .body("paths.'/resource'.get.security[0]", Matchers.hasKey("securityRequirement"))
                 .body("paths.'/resource'.get", Matchers.hasKey("x-openApiExtension"));
+    }
 
+    @Test
+    void testOpenApiOptionsHttpMethod() {
         RestAssured.given()
                 .when().options(OPEN_API_PATH)
-                .then().header("Allow", "GET, HEAD, OPTIONS");
+                .then().header("Allow", "GET, HEAD, OPTIONS")
+                .body(Matchers.is(Matchers.emptyOrNullString()));
+    }
+
+    @Test
+    void testOpenApiUnsupportedHttpMethod() {
+        RestAssured.given()
+                .when().delete(OPEN_API_PATH)
+                .then().statusCode(405);
     }
 }
